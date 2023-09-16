56802a5072533ce3c0711b1cead0b93c373b5f65
ReallyAwesomeName <90869629+ReallyAwesomeName@users.noreply.github.com>
2023-09-16 17:18:40 -0400

    404 workaround

--- src/components/about.js
+++ src/components/about.js
@@ -6,14 +6,14 @@ function About() {
   return (
     <div className="about">
       <h1>About</h1>
-      {/* FIXME: this no work */}
-      {/* <Document
-        file={resume}
-        onLoadError={console.error}
-        style={{ width: "100vw", height: "auto" }}
-      >
-        <Page pageIndex={0} />
-      </Document> */}
+      <p>
+        I was an automotive technician for a few years but concluded it was not
+        where I wanted to be. I found a passion for computing and programming
+        and began my education in computer science. I now have a solid
+        foundation in web development using JavaScript, HTML, CSS, and
+        technologies such as MongoDB, Express.js, React.js and Node.js (MERN
+        Stack). I am now looking to begin my career in web development.
+      </p>
     </div>
   );
 }
